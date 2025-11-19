b234df22429abefa7e61790666b9bb93504cc9b5
Pratibha573 <sypratibha63@gmail.com>
2025-11-19 11:04:35 +0530

    Create cia2.py

---
+++ cia2.py
@@ -0,0 +1,16 @@
+import sys
+
+# If proper 3 parameters are NOT given → use default values
+if len(sys.argv) != 4:
+    print("No proper parameters given. Using default values...")
+    prices = [10.0, 20.0, 30.0]  # default values
+else:
+    prices = [float(sys.argv[1]), float(sys.argv[2]), float(sys.argv[3])]
+
+minimum = min(prices)
+maximum = max(prices)
+average = sum(prices) / len(prices)
+
+print("Minimum price:", minimum)
+print("Maximum price:", maximum)
+print("Average price:", average)
